b2206247c3ae253455101cde711d6c496ebe8da9
Chloe Sauvage <chsauvag@c2r5s1.42luxembourg.lu>
2026-01-20 18:33:54 +0100

    cheh

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,12 +6,30 @@
 /*   By: chsauvag <chsauvag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 12:52:49 by chsauvag          #+#    #+#             */
-/*   Updated: 2026/01/20 18:07:51 by chsauvag         ###   ########.fr       */
+/*   Updated: 2026/01/20 18:33:14 by chsauvag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 class Channel 
 {
-    //-----
-
+    private:
+        //-----basic channel info
+        std::string channelName;
+        std::string topic;
+        
+        //-----client vector
+        std::vector<*Clients> clients;
+        std::vector<*Clients> operators;
+        
+        //-----modes
+        bool inviteOnly; //-i
+        bool topicProtected; //-t
+        std::string key; //-k
+        int userLimit; //-l
+        
+    public:
+        //-----
+        const std::string& getChannelName() const;
+        const std::string& getTopic() const;
+        
 }
