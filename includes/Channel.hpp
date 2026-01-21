a75f067fb239b113f45ef4715c518a388e43ee4e
Chloe Sauvage <chsauvag@c2r9s3.42luxembourg.lu>
2026-01-21 11:08:21 +0100

    added channel class

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: chsauvag <chsauvag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 12:52:49 by chsauvag          #+#    #+#             */
-/*   Updated: 2026/01/21 10:44:23 by chsauvag         ###   ########.fr       */
+/*   Updated: 2026/01/21 11:07:53 by chsauvag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,45 @@ class Channel
         int userLimit; //-l
         
     public:
+        //-----constructors
+        Channel(const std::string& channelName);
+        Channel();
+        ~Channel();
+        
         //-----getters
         const std::string& getChannelName() const;
         const std::string& getTopic() const;
         const std::vector<*Client>& getClients() const;
         const std::vector<*Client>& getOperators() const;
+
+        bool isInviteOnly();
+        bool isTopicProtected();
+        const std::string getKey();
+        int getUserLimit();
+
+        //-----client management
+        void addClient(Client* client);
+        void removeClient(Client* client);
+        void isInChannel(Client* client);
+
+        //-----operator management 
+        void addOperator(Client* operator);
+        void removeOperator(Client* operator);
+        void isOperator(Client* operator);
+
+        //-----broadcast messaging
+        void broadcastMessage(Client* fromWho, const std::string& msg)
         
+        //-----setters (mode options)
+        void setInviteOnly(bool state);
+        void setTopicProtected(bool state);
+        void setKey(std::string& key); //string empty = remove key
+        void setUserLimit(int limit);
+
+        //-----setters (channel info)
+        void setChannelName(std::string& name);
+        void setTopic(std::string& topic);
+
+        #endif
         
 }
